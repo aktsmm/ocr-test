141635651c6cc24802e338f3c7694cd800db2ce1
tatsumiy <tatsumiy@microsoft.com>
2024-08-19 00:38:00 +0900

    chore: Update server and client configurations

--- back/src/main.ts
+++ back/src/main.ts
@@ -6,13 +6,19 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
+  
+  // 環境変数からポート番号を取得
+  const port = configService.get<number>('PORT') || 8000;
+
   const allowedOrigins = configService.get<string>('ALLOWED_ORIGINS')?.split(',') || [];
 
   app.enableCors({
     origin: allowedOrigins,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE, OPTIONS',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
-  })
-  await app.listen(8000);
+  });
+
+  await app.listen(port);
 }
+
 bootstrap();
